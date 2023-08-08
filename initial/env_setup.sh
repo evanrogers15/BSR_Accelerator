4d1f7d4d0a93a813c95d0b2a7b7b87430dde3d31
Evan Rogers <evan.rogers@broadcom.com>
2023-08-08 16:32:51 -0500

    Initial

--- initial/env_setup.sh
+++ initial/env_setup.sh
@@ -4,6 +4,6 @@ mkdir /data
 mkdir /data/influx
 cp -f /initial/bsr_script_interactive.sh /config/bsr_script_interactive.sh
 chmod +x /config/bsr_script_interactive.sh
-cp -f /initial/grafana.bak /config/grafana.ini
-cp -f /initial/telegraf.bak /config/telegraf.conf
-cp -f /initial/influxdb_ds.bak /config/influxdb_ds.yml
+cp -f /initial/grafana.ini /config/grafana.ini
+cp -f /initial/telegraf.conf /config/telegraf.conf
+cp -f /initial/influxdb_ds.yml /config/influxdb_ds.yml
